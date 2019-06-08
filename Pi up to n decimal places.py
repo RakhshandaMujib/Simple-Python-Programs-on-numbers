f7d12562f5ad4bdc597096cba1983fe668826672
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2019-06-08 15:57:47 +0530

    Delete Pi up to n decimal places.py

--- Pi up to n decimal places.py
+++
@@ -1,6 +0,0 @@
-import math
-
-print("Generating pi upto n (cannot exceed 15) number of decimal places.")
-N = int(input("Enter n: "))
-pi = round(math.pi, N)
-print(f"Pi up to {N} decimal places = {pi}")
