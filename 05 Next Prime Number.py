0cc5fcc0444d63c8c1f24e0a6cffbb76b1d72be9
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2019-06-24 20:48:04 +0530

    Update 05 Next Prime Number.py

--- 05 Next Prime Number.py
+++ 05 Next Prime Number.py
@@ -4,18 +4,20 @@ def main():
     
     print("This program will print prime numbers till you want.")
     choice = input("Do you want to see the first prime number?")
-    if choice.lower() == "yes" or choice.lower() == "y":
-        choice = input("2\nAnd the next?")
         
     prime_num = generatePrime()
     
-    while choice.lower() == "yes" or choice.lower() == "y":
+    while choice.lower() == "yes" or choice.lower() == "y": #As long as the user wants, the next prime will be generated.
         print(next(prime_num))
         choice = input("And the next?")
     else:
         print("Okay, thank you!")
         
 def generatePrime():
+    '''
+    Will yield the next prime number. 
+    '''
+    yield 2 #Taking care of 2 separately.
     for num in range (3,100000,2):
         for factor in range(3, int(math.sqrt(num))+1,2):
             if num % factor == 0: 
