320a4f662319885d7ebfbaeeabbbdd84cdc0ec21
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2019-08-05 20:43:30 +0530

    Update 04 Prime Factorization.py

--- 04 Prime Factorization.py
+++ 04 Prime Factorization.py
@@ -3,25 +3,25 @@ import math
 def main():
 	print("Generating the prime factors of a given number.")
 	num = int(input("\nEnter a number: "))
+	
+def prime_factors(n): 
+	count = 0
 
-	def prime_factors(n): 
-		count = 0
+	while n % 2 == 0: #Dividing the number as many times as possible by 2.
+		print(2)
+		n /= 2
+		count += 1 
 
-		while n % 2 == 0: #Dividing the number as many times as possible by 2.
-			print(2)
-			n /= 2
-			count += 1 
+	#Dividing the number by all possible odd numbers (now that the number is no longer an enven number).
+	for num in range(3,int(math.sqrt(n)+1), 2): 
+		while n % num == 0: #As many times as possible by each number. 
+			print(num)
+			n /= num
+			count += 1
 
-		#Dividing the number by all possible odd numbers (now that the number is no longer an enven number).
-		for num in range(3,int(math.sqrt(n)+1), 2): 
-			while n % num == 0: #As many times as possible by each number. 
-				print(num)
-				n /= num
-				count += 1
-
-		if count == 0: # In essence, if none of the loops have ever been executed. Will happen only in case of odd primes. 
-			print(int(n)) 
-	prime_factors(num)
+	if count == 0: # In essence, if none of the loops have ever been executed. Will happen only in case of odd primes. 
+		print(int(n)) 
+prime_factors(num)
 
 if __name__ == '__main__':
 	main()
