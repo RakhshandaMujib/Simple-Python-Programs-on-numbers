4cf82881abd74051431bce135d1b2d082ade6b0c
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2019-08-05 10:28:07 +0530

    Update 03 Fibonacci Sequence.py

--- 03 Fibonacci Sequence.py
+++ 03 Fibonacci Sequence.py
@@ -1,24 +1,23 @@
 def main():
 
 	print("Generating Fibonacci Sequence up to n number of integers.")
-
-	def fibo():
-		'''
-		The Fibonacci series looks like:
-		1 1 2 3 5 8...
-		It basically sums up last two predecessors of the series to form the next successor. 
-		The series starts from either 0 or 1. 
-		'''
-		n = int(input("Enter n: "))
-		a = 1 #Taking the series to start with a 1 
-		b = 1
-
-		for number in range(n):
-			yield a
-			a,b = b,a+b
-
 	for item in fibo():
 		print(item)
+		
+def fibo():
+	'''
+	The Fibonacci series looks like:
+	1 1 2 3 5 8...
+	It sums up last two predecessors of the series to form the next successor. 
+	The series starts from either 0 or 1. 
+	'''
+	n = int(input("Enter n: "))
+	a = 1 #Taking the series to start with a 1 
+	b = 1
 
+	for number in range(n):
+		yield a
+		a,b = b,a+b
+			
 if __name__ == '__main__':
 	main()
